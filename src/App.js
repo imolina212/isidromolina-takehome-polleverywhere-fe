62a0bf110330b1c23ad99540d7e8b9ec4c70da8c
imolina212 <86175612+imolina212@users.noreply.github.com>
2023-01-13 19:58:31 -0500

    add raffle page route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Home from "./pages/home/Home";
+import Raffle from "./pages/raffle/Raffle";
 import "./App.scss";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route exact path="/" element={<Home />} />
+				<Route path="/raffle" element={<Raffle />} />
 			</Routes>
 		</div>
 	);
